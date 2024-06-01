a1fde28ca22f9e30b6e9d33fe584e98ad09ec6fc
elena.alcaraz.martin@gmail.com <elena.alcaraz.martin@gmail.com>
2024-06-01 11:43:00 +0200

    css landing

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import "../styles/Landing.scss";
 
 const Landing = () => {
   return (
-    <div>
-        <Link to="/crear"><button>Crear Proyecto</button></Link>
-        <Link to="/proyectos"><button>Ver otros proyectos</button></Link>
+    <div className="landingDiv">
+
+        <h1 className="landingDiv_h1">Books4You</h1>
+
+        <div className="landingDiv_buttons">
+          <Link to="/crear"><button>Crear Proyecto</button></Link>
+          <Link to="/proyectos"><button>Ver otros proyectos</button></Link>
+        </div>
 
     </div>
   )
