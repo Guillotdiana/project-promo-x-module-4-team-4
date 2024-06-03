2ba14e96e1a96ac9dd1a3af216f5605eadbf17fb
aidanar <aidanarfer@outlook.es>
2024-06-03 12:27:21 +0200

    add mockup

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,13 +1,16 @@
 import CardPreview from "./CardPreview";
-import defaultAvatar from '../images/libro.jpg';
+import defaultAvatar from '../images/mockup-placeholder.png';
 
 const Preview = (props) => {
   const avatar = props.userData.image === '' ? defaultAvatar : props.userData.image;
   return (
     <section className="preview">
+      <div className="mockup"></div>
       <div className="projectImage"
       style={{ backgroundImage: `url(${avatar})` }} ></div>
+      <div className="projectImagePlaceholder"></div>
       <CardPreview userData= {props.userData} />
+      
     </section>
   )
 }
