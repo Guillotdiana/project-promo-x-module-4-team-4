695a167a79981ae4894dc27eec92d4547ca95aea
LuciaLacort <lacortlucia@gmail.com>
2024-06-06 12:24:39 +0200

    changes

--- web/src/components/Landing.jsx
+++ web/src/components/Landing.jsx
@@ -8,17 +8,18 @@ const Landing = () => {
   return (
     <div className="landingDiv">
 
-        <h1 className="landingDiv_h1">Books4You</h1>
-        <h6 className="landingDiv_subtitle">Crea tarjetas para compartir tus libros favoritos o encuentra recomendaciones de otros usuarios</h6>
+        {/* <h1 className="landingDiv_h1">Books4You</h1> */}
+      
 
         <div className="logoDiv">
         <img src={logo} alt="Books4You Logo" className="logoDiv_logo"/>
         </div>
-
+        <h6 className="landingDiv_subtitle">Crea tarjetas para compartir tus libros favoritos o encuentra recomendaciones de otros usuarios</h6>
         <div className='landing__button'>
           <Link to="/crear"><button className="landingDiv_buttons">Crear Tarjeta</button></Link>
           <Link to="/proyectos"><button className="landingDiv_buttons">Ver otros Libros</button></Link>
         </div>
+      
 
         <div className="landingDiv_decoration">
         <img src={landingImage} alt="Imagen de decoración" className="landingDiv_decoration_img"/>
