3aef720d5298396defcd503b80a35e0db4df57a2
aidanar <aidanarfer@outlook.es>
2024-06-10 18:53:03 +0200

    update links

--- web/src/components/List.jsx
+++ web/src/components/List.jsx
@@ -80,7 +80,7 @@ const List = () => {
       </form>
       <div className='list-page' >
         <div className='cards-container'>
-          {filterBook.map((userData,i) => <Link className="card-link" to={`/detailBook/${userData.idBook}`} key={i} target="_blank"><CardPreview  userData={userData}/></Link>)}
+          {filterBook.map((userData,i) => <a className="card-link" href={`http://localhost:5001/detailBook/${userData.idBook}`} key={i} target="_blank"><CardPreview  userData={userData}/></a>)}
           
         </div>
         <div>
